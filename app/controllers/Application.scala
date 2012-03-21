b7834fbf9e22928c3abc181ca79020bebfc53c82
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-21 18:14:57 +0100

    Add OAuth authentication

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.oauth._
 
-object Application extends Controller {
-  
-  def index = Action {
+object Application extends Controller with OAuthAuthentication {
+
+  def index = Authenticated { tokens => request =>
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  val authenticateCall = routes.Application.authenticate
+  val authenticatedCall = routes.Application.index
+  val oAuth = OAuth(ServiceInfo("https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
+    "https://api.twitter.com/oauth/authorize",
+    ConsumerKey("yzfT2vhgsBjgWEquQHSCsg", "mtVRo5tJ0xJ4ZK8l7pAowaYjMSZRGdYY9NphtWVp5dA")))
+    //ConsumerKey("UstozDw940RBShjqOhNQ", "DgJXSk2HAuDUaqwDqX8L6wb7Q15EGrzeT7qEJzACLA")))
 }
---
+++ app/controllers/OAuthAuthentication.scala
@@ -0,0 +1,54 @@
+package controllers
+
+import play.api.libs.oauth._
+import play.api.mvc._
+import play.api.Logger
+
+trait OAuthAuthentication extends Controller {
+
+  def authenticateCall: Call
+  def authenticatedCall: Call
+  def oAuth: OAuth
+
+  def authenticate = Action { implicit request =>
+
+    val verifierAndTokens = for {
+      values <- request.queryString.get("oauth_verifier")
+      verifier <- values.headOption
+      tokens <- sessionTokens
+    } yield (verifier, tokens)
+
+    val result = verifierAndTokens match {
+      case None => {
+        // First access: try to retrieve request tokens
+        for (tokens <- oAuth.retrieveRequestToken(authenticateCall.absoluteURL()).right) yield {
+          Redirect(oAuth.redirectUrl(tokens.token))
+              .withSession("token"->tokens.token, "secret"->tokens.secret)
+        }
+      }
+      case Some((verifier, tokens)) => {
+        // Second time: the user has been authenticated, retrieve the access tokens
+        for (tokens <- oAuth.retrieveAccessToken(tokens, verifier).right) yield {
+          Redirect(authenticatedCall)
+              .withSession("token"->tokens.token, "secret"->tokens.secret)
+        }
+      }
+    }
+    result.right.getOrElse(InternalServerError("Authentication failed!"))
+  }
+
+  def Authenticated(action: RequestToken => RequestHeader => Result) = Action { implicit request =>
+    sessionTokens match {
+      case Some(tokens) => action(tokens)(request)
+      case None => Redirect(authenticateCall)
+    }
+  }
+
+  def sessionTokens(implicit request: RequestHeader) = {
+    for {
+      token <- session.get("token")
+      secret <- session.get("secret")
+    } yield RequestToken(token, secret)
+  }
+
+}
