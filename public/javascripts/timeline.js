89e18966003153fd1cf26b4e8a9e25e5228df428
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-30 11:43:32 +0200

    use form to search for tweets

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -1,11 +1,21 @@
-(function() {
+;(function() {
 
-  var source = new EventSource("/tweets/javascript")
-  source.onmessage = function(event) {
-    console.log(event.data)
-  }
-  source.onerror = function(event) {
-    console.log("EventSource fired an error")
-  }
+  var input = document.querySelector('input'),
+      source;
 
-}())
+  input.addEventListener('keydown', function (e) {
+    if (e.keyCode == 13) { // Enter has been pressed
+      source && source.close();
+      source = new EventSource(Routes.controllers.Application.tweets(input.value).url);
+      source.onmessage = function (e) {
+        console.log(e.data);
+      };
+      source.onerror = function (e) {
+        console.log("error", e)
+      };
+
+      input.value = '';
+    }
+  });
+
+}());
