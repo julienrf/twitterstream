3e6ca469fe7d51f1f5e86b8eb33954172731f447
Julien Richard-Foy <julien@richard-foy.fr>
2012-04-25 15:04:16 +0200

    shame on me

--- app/controllers/Twitter.scala
+++ app/controllers/Twitter.scala
@@ -1,12 +1,13 @@
 package controllers
 
 import play.api.libs.iteratee.Iteratee
+import play.api.libs.concurrent.Promise
 import play.api.libs.ws.{WS, ResponseHeaders}
 import play.api.libs.oauth.{OAuthCalculator, ConsumerKey, RequestToken}
 
 trait Twitter {
 
-  def tweetsStream[A](token: RequestToken)(terms: String)(consume: ResponseHeaders => Iteratee[Array[Byte], A]) {
+  def tweetsStream[A](token: RequestToken)(terms: String)(consume: ResponseHeaders => Iteratee[Array[Byte], A]): Promise[Iteratee[Array[Byte], A]] = {
     WS.url("https://stream.twitter.com/1/statuses/filter.json?track=" + terms)
       .sign(OAuthCalculator(consumerKey, token))
       .get(consume)
