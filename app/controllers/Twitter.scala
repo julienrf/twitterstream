d95327312192c5419e295d4e8dc7f2783b5af1e9
Julien Richard-Foy <julien@richard-foy.fr>
2012-04-25 14:57:11 +0200

    Use imperative notation

--- app/controllers/Twitter.scala
+++ app/controllers/Twitter.scala
@@ -6,7 +6,7 @@ import play.api.libs.oauth.{OAuthCalculator, ConsumerKey, RequestToken}
 
 trait Twitter {
 
-  def tweetsStream[A](token: RequestToken)(terms: String)(consume: ResponseHeaders => Iteratee[Array[Byte], A]) = {
+  def tweetsStream[A](token: RequestToken)(terms: String)(consume: ResponseHeaders => Iteratee[Array[Byte], A]) {
     WS.url("https://stream.twitter.com/1/statuses/filter.json?track=" + terms)
       .sign(OAuthCalculator(consumerKey, token))
       .get(consume)
